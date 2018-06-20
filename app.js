c0ca96bd1565f321997d16227aa2d54f7064e4e3
Jonathan James <jjamesnwa1011@gmail.com>
2018-06-19 20:09:45 -0700

    fixed voting route by adding a middleware function to filter out any requests other than those ending in vot-up or vote-down

--- app.js
+++ app.js
@@ -11,7 +11,28 @@ app.use(jsonParser());
 //only direct routes that start with /questions through our routes.js file
 app.use("/questions", routes);
 
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
+
+
+
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
-  console.log('Express server is listening on port', port)
+  console.log('Express server is listening on port', port);
 });
