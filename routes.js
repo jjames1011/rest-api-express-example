037314fd2b78a0c4d3a4f34e16d6a6176a942135
Jonathan James <jjamesnwa1011@gmail.com>
2018-06-19 19:35:42 -0700

    added some starter routes

--- routes.js
+++ routes.js
@@ -2,5 +2,28 @@
 var express = require("express");
 var router = express.Router();
 
+//GET /questions
+// Return all the questions
+
+router.get("/", (req, res) => {
+  res.json({response: "You sent me a GET request"});
+});
+
+//POST /questions
+// Route for creating questions
+router.post("/", (req, res) => {
+  res.json({
+    response: "You sent me a POST request",
+    body: req.body
+  });
+});
+
+//GET /questions/:id
+// Route for specific questions
+router.get("/:id", (req, res) => {
+  res.json({
+    response: "You sent me a GET request for ID " + req.params.id
+  })
+})
 
 module.exports = router;
